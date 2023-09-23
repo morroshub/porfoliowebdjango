035ba216c409b6fe10f7384742d47988ad49d083
morroshub <lgmorrone@icloud.com>
2023-09-23 20:55:48 -0300

    problemas de inconsistencia con el css

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,4 @@ def render_posts(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {'posts': post_id}) 
+    return render(request, 'post_detail.html', {'post': post})
