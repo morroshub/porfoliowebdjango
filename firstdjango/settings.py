9159fc916960e622884bf4a92e465a7bd68df716
morroshub <lgmorrone@icloud.com>
2023-10-10 19:15:56 -0300

    se instalo gunicorn y fly, se modifico el archivo settings.py, se procede a desplegar el site

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -127,4 +127,5 @@ MEDIA_URL = '/public/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ["*"]  
