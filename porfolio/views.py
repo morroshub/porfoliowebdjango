7ba99894a96ef05f2e0d116d672d2e3a68d11ab9
morroshub <lgmorrone@icloud.com>
2024-01-31 12:31:36 -0300

    agregamos un init url donde se podra elegir a que seccion entrar, algo similar a un linktree, separamos el home de morrospace para diferenciar los urls y sus propositos

--- porfolio/views.py
+++ porfolio/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Project
 
+
+# Funcion de inicio de la web ; nos envia a cada apartado a un estilo linktree
+def init_view(request):
+    return render(request, 'init.html')
+
+# Funcion que llama a los projectos dentro de home
 def home(request):
     projects = Project.objects.all()
     return render(request, 'home.html', {'projects': projects})
+
+# Funcion que llama a Morrospacehtml
+def Morrospace(request):
+    return render(request, 'Morrospace.html')
