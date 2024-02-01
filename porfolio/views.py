cacdd0e5b76fff35f29dfe9899676059e13325f3
morroshub <lgmorrone@icloud.com>
2024-02-01 16:43:45 -0300

    manejo de errores

--- porfolio/views.py
+++ porfolio/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from .models import Project
+from django.shortcuts import render
+# from django.shortcuts import redirect para errores 3xx
+from django.http import HttpResponseNotFound, HttpResponseServerError # Manejo de errores 
 
 
 # Funcion de inicio de la web ; nos envia a cada apartado a un estilo linktree
@@ -18,6 +20,32 @@ def Morrospace(request):
 
 def index(request):
     return render(request, "layout.html")
+
+
+# Manejo de errores :
+
+# # 3xx
+# def custom_301(request):
+#     return redirect('nueva_ruta', permanent=True)
+
+# def custom_302_with_message(request):
+#     context = {'mensaje': 'Esta página se ha movido temporalmente.'}
+#     return render(request, '302_with_message.html', context, status=302)
+
+
+# 4xx
+def custom_403(request, exception=None):
+    return render(request, '403.html', status=403)
+
+def custom_404(request, exception=None):
+    return render(request, '404.html', status=404)
+
+
+# 5xx
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
+
 
 
 def your_view(request):
