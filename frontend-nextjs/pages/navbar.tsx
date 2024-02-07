573197e31e7cc8e7255f625e7bd5af07b482bbd9
morroshub <lgmorrone@icloud.com>
2024-02-07 18:40:56 -0300

    se realizo el serealizado de blog

---
+++ frontend-nextjs/pages/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.nav`
+    /* Estilos de tu navbar */
+`;
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            {/* Contenido de tu navbar */}
+        </nav>
+    );
+};
+
+export default Navbar;
